fa7e009345beb9162ea5b70389ed76f7fba6905d
Jacint Blanch <jacintblanch5@gmail.com>
2023-07-25 07:37:24 +0200

    Make characters structure

---
+++ src/ts/characters/King/King.ts
@@ -0,0 +1,15 @@
+import { type CharacterData } from "../../types.js";
+import Character from "../Character/Character.js";
+
+export class King extends Character {
+  yearsOfReign;
+
+  constructor(characterData: CharacterData, yearsOfReign: number) {
+    super(characterData);
+    this.yearsOfReign = yearsOfReign;
+  }
+
+  communicate() {
+    return `${super.communicate()}Everybody will die`;
+  }
+}
